ae6c8ae213cf2e6de6770da84a277070277b42e2
Jexus Chuang <b05901033@ntu.edu.tw>
2020-03-22 18:33:33 +0800

    Upload dataset in another position

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,5 +1,5 @@
 # Download dataset from Google Drive
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1daFU8tPPUyhN7Fc6JUTohEfHXIn6ZDgq' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1daFU8tPPUyhN7Fc6JUTohEfHXIn6ZDgq" -O HW1-data.tar.gz && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1BPR3IfAEOFOQzsU4vDs2fezDyEsU7bvQ' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1BPR3IfAEOFOQzsU4vDs2fezDyEsU7bvQ" -O HW1-data.tar.gz && rm -rf /tmp/cookies.txt
 
 # Unzip the dataset
 tar zxvf HW1-data.tar.gz --no-same-owner > log && rm log
